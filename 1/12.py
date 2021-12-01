5395b61320b43e7fecc291e0c854a0db3efdaa6f
Mattias Ahle <mattias.ahle@gmail.com>
2021-12-01 14:31:42 +0100

    Cleanup

--- 1/12.py
+++ 1/12.py
@@ -1,12 +1,12 @@
-count = 1
+count = 0
 
-with open('input.txt', 'r') as f:
+with open("input.txt", "r") as f:
     lines = list(map(int, f.readlines()))
 
 prev_sum = sum(lines[0:3])
 
-for i in range(3, len(lines)-1):
-    new_sum = sum(lines[i-2:i+1])
+for i in range(4, len(lines) + 1):
+    new_sum = sum(lines[i - 3 : i])
 
     if new_sum > prev_sum:
         count = count + 1
