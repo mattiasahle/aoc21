2e0853acf3eb588ea9e63418252f6086611607b3
Mattias Ahle <mattias.ahle@gmail.com>
2021-12-01 14:09:45 +0100

    Task 1.2 cleanup

--- 1/12.py
+++ 1/12.py
@@ -3,14 +3,15 @@ count = 1
 with open('input.txt', 'r') as f:
     lines = list(map(int, f.readlines()))
 
-prev_sum = lines[0] + lines[1] + lines[2]
-sum = lines[1] + lines[2] + lines[3]
+prev_sum = sum(lines[0:3])
 
-for i in range(3, len(lines) - 1):
-    prev_sum = prev_sum + lines[i] - lines[i-3]
-    sum = sum + lines[i+1] - lines[i-2]
+for i in range(3, len(lines)-1):
+    new_sum = sum(lines[i-2:i+1])
 
-    if sum > prev_sum:
+    if new_sum > prev_sum:
         count = count + 1
+
+    prev_sum = new_sum
+    new_sum = 0
 
 print(count)
