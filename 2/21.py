4ca398ab51327db4615192d0754308fe339132f6
Mattias Ahle <mattias.ahle@gmail.com>
2021-12-02 06:41:48 +0100

    2.2 finish

---
+++ 2/21.py
@@ -0,0 +1,19 @@
+horizontal = 0
+depth = 0
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    
+# print(type(int(lines[0][-2])))
+
+for i in range(len(lines)):
+    value = [int(s) for s in lines[i].split() if s.isdigit()]
+
+    if 'forward' in lines[i]:
+        horizontal = horizontal + value[0]
+    if 'down' in lines[i]:
+        depth = depth + value[0]
+    if 'up' in lines[i]:
+        depth = depth - value[0]
+
+print(horizontal * depth)
---
+++ 2/22.py
@@ -0,0 +1,21 @@
+horizontal = 0
+depth = 0
+aim = 0
+
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+for i in range(len(lines)):
+    value = [int(s) for s in lines[i].split() if s.isdigit()]
+
+    if 'down' in lines[i]:
+        aim = aim + value[0]
+        # depth = depth + value[0]
+    if 'up' in lines[i]:
+        aim = aim - value[0]
+        # depth = depth - value[0]
+    if 'forward' in lines[i]:
+        horizontal = horizontal + value[0]
+        depth = depth + aim * value[0]
+
+print(horizontal * depth)
