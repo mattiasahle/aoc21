7d93683fa5ca116608c8c07e6fe5b7c7978f372f
Mattias Ahle <mattias.ahle@gmail.com>
2021-12-03 07:14:42 +0100

    3.1 finish Python

--- 3/31.py
+++ 3/31.py
@@ -1,4 +1,25 @@
 with open("input.txt", "r") as f:
     lines = f.readlines()
 
-print(lines[0])
+zeros = [0,0,0,0,0,0,0,0,0,0,0,0]
+ones = [0,0,0,0,0,0,0,0,0,0,0,0]
+epsilon = ""
+gamma = ""
+
+for line in lines:
+    for i in range(len(line)):
+        if line[i] == '0':
+            zeros[i] = zeros[i] + 1
+        if line[i] == '1':
+            ones[i] = ones[i] + 1
+
+for i in range(len(zeros)):
+    if zeros[i] > ones[i]:
+        epsilon = epsilon + "1"
+        gamma = gamma + "0"
+    else:
+        epsilon = epsilon + "0"
+        gamma = gamma + "1"
+
+print(int(epsilon,2)*int(gamma,2))
+    
