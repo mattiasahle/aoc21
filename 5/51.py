1d232b471bea666a8443433f9a8e458a318f6c98
Mattias Ahle <mattias.ahle@gmail.com>
2021-12-07 13:12:22 +0100

    5.1 finish Python

--- 5/51.py
+++ 5/51.py
@@ -17,12 +17,30 @@ for line in range(len(coordinates_temp)):
     coordinates[line][1] = tuple(map(int, coordinates_temp[line][1].split(',')))
 
 
-# Remove non-horizontal or non-vertical lines
-i = 1
-for line in coordinates:
-    if line[0][0] != line[1][0] and line[0][1] != line[1][1]:
-        coordinates.remove(line)
-        print(i, line)
-        i = i + 1
+# Task
+grid = [[0 for _ in range(1000)] for _ in range(1000)]
 
-print(len(coordinates))
+for line in coordinates:
+    if line[0][0] == line[1][0] or line[0][1] == line[1][1]:
+        min_x = min(line[0][0], line[1][0])
+        max_x = max(line[0][0], line[1][0])
+        min_y = min(line[0][1], line[1][1])
+        max_y = max(line[0][1], line[1][1])
+
+        if min_x == max_x:
+            for i in range(min_y, max_y + 1):
+                # print(min_x, max_x, min_y, max_y, i)
+                grid[min_x][i] = grid[min_x][i] + 1
+        else:
+            for i in range(min_x, max_x + 1):
+                # print(min_x, max_x, min_y, max_y, i)
+                grid[i][min_y] = grid[i][min_y] + 1
+
+
+sum = 0
+for i in grid:
+    for j in i:
+        if j > 1:
+            sum = sum + 1
+
+print(sum)
